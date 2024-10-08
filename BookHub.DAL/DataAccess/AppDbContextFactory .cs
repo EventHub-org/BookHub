829120b7e50d24541fc152ba33d8c26734edbf78
PavloButynets <123271613+PavloButynets@users.noreply.github.com>
2024-10-08 14:00:36 +0300

    Added the entities folder, created entities for achievements, users, … (#18)
    * Added the entities folder, created entities for achievements, users, collections and friendships in it;
    created the IRepository interface and the methods in it
    created and configured the AppDbContext file
    created and configured the AppDbContext file, added the created tables to it
    
    * added the AppDbContextFactory class for creating AppDbContext instances, connected the project to MsSql, performed migration and created database tables, added restrictions on table columns
    
    * Added table to implement friendship, added DATABASE_CONNECTION_STRING to .env file
    
    * added other entities
    
    * merged conflicts
    
    * Fix Bugs
    
    ---------
    
    Co-authored-by: kriperAsasin2007 <119786509+kriperAsasin2007@users.noreply.github.com>

---
+++ BookHub.DAL/DataAccess/AppDbContextFactory .cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DotNetEnv;
+using System.IO; 
+
+namespace BookHub.DAL.DataAccess
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            string envPath = Path.Combine(Directory.GetCurrentDirectory(), "..\\.env");
+
+            DotNetEnv.Env.Load(envPath);
+            Env.Load();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string not found in environment variables.");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString); 
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ BookHub.DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BookHub.DAL.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+    }
+}
