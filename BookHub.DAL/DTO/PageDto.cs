4ca276cd96a23b1c1913275f96bda5c34a4c6d80
nazariusPr <protsnazar2004@gmail.com>
2024-10-14 22:05:53 +0300

    configure mapper and dto

--- BookHub.DAL/DTO/PageDto.cs
+++ BookHub.DAL/DTO/PageDto.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BookHub.DAL.DTO
+﻿namespace BookHub.DAL.DTO
 {
-    internal class PageDto
+    public class PageDto<T>
     {
+        public List<T> Items { get; set; }
+        public long TotalElements { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public PageDto()
+        {
+            Items = new List<T>();
+        }
+
+        public PageDto(List<T> items, long totalElements, int currentPage, int totalPages)
+        {
+            Items = items;
+            TotalElements = totalElements;
+            CurrentPage = currentPage;
+            TotalPages = totalPages;
+        }
     }
 }
