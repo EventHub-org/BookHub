f3ddca2c6cee6bae5cad2da6bba3476472cfe471
PavloButynets <pavlobutynez@gmail.com>
2024-10-15 02:17:09 +0300

    Implemented a use case for creating a collection, Added CollectionMapper, CollectionService, CollectionDto and tests for ConnectionService

---
+++ BookHub.DAL/Mappers/CollectionProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BookHub.DAL.DTO;
+using BookHub.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHub.DAL.Mappers
+{
+    public class CollectionProfile : Profile
+    {
+        public CollectionProfile()
+        {
+            CreateMap<CollectionEntity, CollectionDto>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.UserId)) // Витягуємо UserId з User
+                .ReverseMap()
+                .ForMember(dest => dest.User, opt => opt.Ignore()); // Ігноруємо User під час мапування назад
+        }
+    }
+
+}
