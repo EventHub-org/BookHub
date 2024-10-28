5fee54cd2d7148d231bd4c4b9c11164bba19f0a3
nazariusPr <protsnazar2004@gmail.com>
2024-10-28 20:05:24 +0200

    Create Pageable.cs

---
+++ BookHub.DAL/DTO/Pageable.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BookHub.DAL.DTO
+{
+    public class Pageable
+    {
+        public int Size { get; set; }
+        public int Page { get; set; }
+
+        public Pageable(int size, int page)
+        {
+            Size = size;
+            Page = page;
+        }
+
+        public Pageable() : this(10, 0)
+        {
+        }
+    }
+}
