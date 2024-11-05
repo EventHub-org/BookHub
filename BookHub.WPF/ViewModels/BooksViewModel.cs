21e97c52c72e29221b3a2868d6a350814e5b32b0
Stanislav Topilnytskyi <stastopilnytskii@gmail.com>
2024-11-05 11:53:43 +0200

    .

--- BookHub.WPF/ViewModels/BooksViewModel.cs
+++ BookHub.WPF/ViewModels/BooksViewModel.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BookHub.WPF.ViewModels
 {
@@ -15,11 +16,17 @@ namespace BookHub.WPF.ViewModels
         private readonly IBookService _bookService;
         private ObservableCollection<BookDto> _books;
         private BookDto _selectedBook;
+        private int _currentPage;
+        private const int _pageSize = 3; // Кількість книг на сторінці
+        private int _totalPages;
 
         public BooksViewModel(IBookService bookService)
         {
             _bookService = bookService;
+            CurrentPage = 1;
             LoadBooksAsync().ConfigureAwait(false);
+            PreviousPageCommand = new RelayCommand(PreviousPage, CanGoToPreviousPage);
+            NextPageCommand = new RelayCommand(NextPage, CanGoToNextPage);
         }
 
         public ObservableCollection<BookDto> Books
@@ -42,14 +49,59 @@ namespace BookHub.WPF.ViewModels
             }
         }
 
+        public int CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+                LoadBooksAsync().ConfigureAwait(false);
+            }
+        }
+
+        public int TotalPages
+        {
+            get => _totalPages;
+            private set
+            {
+                _totalPages = value;
+                OnPropertyChanged(nameof(TotalPages));
+            }
+        }
+
+        public ICommand PreviousPageCommand { get; }
+        public ICommand NextPageCommand { get; }
+
         private async Task LoadBooksAsync()
         {
-            var result = await _bookService.GetPaginatedBooksAsync(new Pageable { Page = 1, Size = 10 });
+            var result = await _bookService.GetPaginatedBooksAsync(new Pageable { Page = CurrentPage, Size = _pageSize });
             if (result.Success)
             {
                 Books = new ObservableCollection<BookDto>(result.Data.Items);
+                TotalPages = result.Data.TotalPages; // Припустимо, ваш сервіс повертає загальну кількість сторінок
             }
         }
+
+        private void PreviousPage()
+        {
+            if (CanGoToPreviousPage())
+            {
+                CurrentPage--;
+            }
+        }
+
+        private void NextPage()
+        {
+            if (CanGoToNextPage())
+            {
+                CurrentPage++;
+            }
+        }
+
+        private bool CanGoToPreviousPage() => CurrentPage > 1;
+
+        private bool CanGoToNextPage() => CurrentPage < TotalPages;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -58,5 +110,4 @@ namespace BookHub.WPF.ViewModels
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-
 }
